c8d37b6684721bc6508c7be09b9bacfacb104c7a
Adrian G Prado <adrian.garciaprado@nrg.com>
2016-10-09 17:48:37 -0500

    Move playbooks from sub-folders

--- scripts/deploy-gitlab-ci.sh
+++ scripts/deploy-gitlab-ci.sh
@@ -4,4 +4,4 @@ FILE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 . ${FILE_DIR}/init.sh
 
 ansible-playbook \
-  -i ${inventory} ${PLAYBOOKS_DIR}/gitlab/gitlab-ci.yml "$@"
+  -i ${inventory} ${PLAYBOOKS_DIR}/gitlab-ci.yml "$@"
